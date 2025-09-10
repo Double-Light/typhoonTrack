69f368c84fdb4afd223c5339038e7aa5c3d3932e
Double-Light <103574126+Double-Light@users.noreply.github.com>
2025-09-10 16:01:03 +0800

    Add files via upload

--- js/set_Trigger_Conditions.js
+++ js/set_Trigger_Conditions.js
@@ -614,17 +614,11 @@ async function canvasToBlob(canvas) {
   return new Promise(resolve => canvas.toBlob(resolve, "image/png"));
 }
 
-function dataURLToBlob(dataUrl) {
-  const arr = dataUrl.split(',');
-  const mime = arr[0].match(/:(.*?);/)[1];
-  const bstr = atob(arr[1]);
-  let n = bstr.length;
-  const u8arr = new Uint8Array(n);
-  while (n--) {
-    u8arr[n] = bstr.charCodeAt(n);
-  }
-  return new Blob([u8arr], {
-    type: mime
+function blobToDataUrl(blob) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.readAsDataURL(blob);
   });
 }
 
